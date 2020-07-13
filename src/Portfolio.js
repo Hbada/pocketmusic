775c01aaacc41aa222f85b4bb27a38d141ef3d93
Heidi Bada <heidibada@gmail.com>
2020-07-13 16:38:20 -0700

    tidied up projects1 default state data

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import Pokedex from './Pokedex';
 
-var EmilyBlog = require('./img/EmilyBlog.png');
-var GhoulGame = require('./img/GhoulGame.png');
+var selectSound = require('./img/EmilyBlog.png');
+var turnOff = require('./img/GhoulGame.png');
 
 class Portfolio extends Component {
     static defaultProps = {
         projects1 : [
-            { id: 1, name: 'Turn on', type: 'Put AAA batteries in. If already done, press a button to wake device up.', year: 2018, base_experience: 1, altText: '' },
-            { id: 2, name: 'Select sound', type: 'Press number 1 to 16 to select a sound. Start a pattern first if you want to play along.', year: 2019, base_experience: 1, img: EmilyBlog, altText: '' },
-            { id: 3, name: 'Set volume', type: 'Press BPM + any numbered key 1 to 16. Low is 1, max is 16.', year: 2019, base_experience: 1, altText: '' },
-            { id: 4, name: 'Play pattern', type: 'Hold Pattern + number 1 to 16 to select pattern, then Play to play it.', year: 2016, base_experience: 1, altText: '' },
-            { id: 5, name: 'Turn off', type: 'Press sound + BPM at same time', year: 2018, base_experience: 1, img: GhoulGame },
-            { id: 6, name: '', type: '', year: 2019, base_experience: 1 }
+            { id: 1, name: 'Turn on', type: 'Put AAA batteries in. If already done, press a button to wake device up.', altText: '' },
+            { id: 2, name: 'Select sound', type: 'Press number 1 to 16 to select a sound. Start a pattern first if you want to play along.', img: selectSound, altText: '' },
+            { id: 3, name: 'Set volume', type: 'Press BPM + any numbered key 1 to 16. Low is 1, max is 16.', altText: '' },
+            { id: 4, name: 'Play pattern', type: 'Hold Pattern + number 1 to 16 to select pattern, then Play to play it.', altText: '' },
+            { id: 5, name: 'Turn off', type: 'Press sound + BPM at same time', img: turnOff },
+            { id: 6, name: '', type: '' }
         ],
         projects2 : [
             { id: 1, name: 'Turn on', type: 'Put AAA batteries in. If already done, press a button to wake device up.', year: 2018, base_experience: 1, altText: '' },
