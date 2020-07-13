034b6993f0d642d830d6daf5cf002b632d14fc1b
Heidi Bada <heidibada@gmail.com>
2020-07-13 16:09:20 -0700

    last commit fixed problem. Now trying to make two sets of different cards

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -6,7 +6,15 @@ var GhoulGame = require('./img/GhoulGame.png');
 
 class Portfolio extends Component {
     static defaultProps = {
-        projects : [
+        projects1 : [
+            { id: 1, name: 'Turn on', type: 'Put AAA batteries in. If already done, press a button to wake device up.', year: 2018, base_experience: 1, altText: '' },
+            { id: 2, name: 'Select sound', type: 'Press number 1 to 16 to select a sound. Start a pattern first if you want to play along.', year: 2019, base_experience: 1, img: EmilyBlog, altText: '' },
+            { id: 3, name: 'Set volume', type: 'Press BPM + any numbered key 1 to 16. Low is 1, max is 16.', year: 2019, base_experience: 1, altText: '' },
+            { id: 4, name: 'Play pattern', type: 'Hold Pattern + number 1 to 16 to select pattern, then Play to play it.', year: 2016, base_experience: 1, altText: '' },
+            { id: 5, name: 'Turn off', type: 'Press sound + BPM at same time', year: 2018, base_experience: 1, img: GhoulGame },
+            { id: 6, name: '', type: '', year: 2019, base_experience: 1 }
+        ],
+        projects2 : [
             { id: 1, name: 'Turn on', type: 'Put AAA batteries in. If already done, press a button to wake device up.', year: 2018, base_experience: 1, altText: '' },
             { id: 2, name: 'Select sound', type: 'Press number 1 to 16 to select a sound. Start a pattern first if you want to play along.', year: 2019, base_experience: 1, img: EmilyBlog, altText: '' },
             { id: 3, name: 'Set volume', type: 'Press BPM + any numbered key 1 to 16. Low is 1, max is 16.', year: 2019, base_experience: 1, altText: '' },
@@ -17,8 +25,8 @@ class Portfolio extends Component {
     };
 
     render() {
-        let hand1 = [...this.props.projects ];
-        let hand2 = [...this.props.projects ];
+        let hand1 = [...this.props.projects1 ];
+        let hand2 = [...this.props.projects2 ];
         // while( hand1.length < hand2.length ) {
             // let randIdx = Math.floor(Math.random() * hand2.length);
             // let randprojects = hand2.splice(randIdx, 1)[0];
