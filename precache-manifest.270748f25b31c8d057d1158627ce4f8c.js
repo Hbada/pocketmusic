3c1d102a697998d2f581955b349c60c6d89b1134
Heidi Bada <heidibada@gmail.com>
2020-07-13 15:34:58 -0700

    Updates

---
+++ precache-manifest.270748f25b31c8d057d1158627ce4f8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66865ccebd41e813ce0d6b1a87ee0dae",
+    "url": "/pocketmusic/index.html"
+  },
+  {
+    "revision": "5239dfbd0f44206ab045",
+    "url": "/pocketmusic/static/css/main.15289053.chunk.css"
+  },
+  {
+    "revision": "d29373793a9bea5e0219",
+    "url": "/pocketmusic/static/js/2.a0770525.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/pocketmusic/static/js/2.a0770525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5239dfbd0f44206ab045",
+    "url": "/pocketmusic/static/js/main.a9d4e818.chunk.js"
+  },
+  {
+    "revision": "8c5b93aa6bc6661d3e81",
+    "url": "/pocketmusic/static/js/runtime-main.bed6230d.js"
+  },
+  {
+    "revision": "2ac90392960e72469c936c5c1877799c",
+    "url": "/pocketmusic/static/media/EmilyBlog.2ac90392.png"
+  },
+  {
+    "revision": "64c9ab7c2986fd6be0e0c17b95c00d22",
+    "url": "/pocketmusic/static/media/GhoulGame.64c9ab7c.png"
+  }
+]);
